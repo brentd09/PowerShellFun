097a7ae424861babbb9dada07ed6f3bc9fc499cb
Brent Denny <brentdenny@gmail.com>
2018-02-16 10:20:47 +1000

    BattleShipV2 created logic to continue on a line once found, still needs more logic to stop searching around the line once there is MHHHM type line on the line we were just solving

--- BattleShip/Play-BattleShipv2.ps1
+++ BattleShip/Play-BattleShipv2.ps1
@@ -176,6 +176,21 @@ function Find-LikelyHits {
     $Choice = $null
     $Random = $true
   }
+  #still needs more logic to stop checking once a line is completed...
+  :AllForLoops foreach ($Hit in $Hits) {
+    foreach ($Direction in $Hit.NSEW) {
+      if ($Grid[$Direction].Ship -eq "X") {
+        $PossibleHitPos = (2 * ($Hit.pos)) - $Direction
+        if ($PossibleHitPos -in $Hit.NSEW) {
+          if ($Grid[$PossibleHitPos].PegChosen -eq $false) {
+            $Choice = $PossibleHitPos
+            $Random = $false
+            break AllForLoops
+          }
+        } 
+      }
+    }
+  }
   [PSCustomObject][Ordered]@{
     Choice = $Choice
     Random = $Random
@@ -192,8 +207,8 @@ function Guess-Move {
   if ($ComputersTurn -eq $true) {
     $Likely = Find-LikelyHits -Grid $OpponentGrid.psobject.copy()
     if ($Likely.Random -eq $true) {
-      #$GuessArray = @(1,3,5,7,9,13,16,21,23,25,27,29,33,36,40,41,43,45,47,49,60,61,63,65,67,69,80,81,83,85,87,89,92,94,96,98)
-      $GuessArray = @(0,3,6,9,11,14,17,22,25,28,30,33,36,39,41,44,47,52,55,58,60,63,66,69,71,74,77,82,85,88,90,93,96,99)
+      $GuessArray = @(0,2,4,6,8,11,13,15,17,19,20,22,24,26,28,31,33,35,37,39,40,42,44,46,48,51,53,55,57,59,60,62,64,66,68,71,73,75,77,79,80,82,84,86,88,91,93,95,97,99)
+      #$GuessArray = @(0,3,6,9,11,14,17,22,25,28,30,33,36,39,41,44,47,52,55,58,60,63,66,69,71,74,77,82,85,88,90,93,96,99)
       $PosRemaining = ($OpponentGrid | where {$_.Pegchosen -eq $false -and $_.Pos -in $GuessArray} ).Pos
       if ($PosRemaining.count -eq 0) {$PosRemaining = ($OpponentGrid | Where-Object {$_.PegChosen -eq $false}).Pos }
       $GuessPos = $PosRemaining | get-random
@@ -218,7 +233,7 @@ function Guess-Move {
     do {
       do {
         Write-Host
-        Write-Host -ForegroundColor Yellow "Enter the coordinates for next attack in the form D7 or 7D"
+        Write-Host -ForegroundColor Yellow -NoNewline "Enter the coordinates for next attack in the form D7 or 7D: "
         $Guess = Read-Host 
         $Guess = ($Guess -replace "[^A-J0-9]",'').ToUpper()
         if ($Guess -match "^[0-9][A-J]$") {$Guess = $Guess -replace "^(.)(.)$",'$2$1'}
