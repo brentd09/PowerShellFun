03907f480a030c3939c39ed3569c93b4e5df722a
brent denny <brentdenny@gmail.com>
2018-04-04 06:41:49 +1000

    sudoku2 - setting up hidden cell function

--- Sudoku2/Sudoku2.ps1
+++ Sudoku2/Sudoku2.ps1
@@ -1,7 +1,7 @@
 [Cmdletbinding()]
 Param (
   [ValidateLength(81,81)]
-  [string]$SudokuBoard = '89-2-3-------------3658---41-8-3--6-----------2--7-3-57---9412-------------8-2-59'
+  [string]$SudokuBoard = '--5--7--4-6--5--9-4--9--2--2--5--1---7--2--4---8--3--2--7--1--3-5--6--1-6--8--4--'
 )
 # Easy       '-6-3--8 4537-9-----4---63-7-9..51238---------71362--4-3-64---1-----6-5231-2--9-8-'
 # Medium     '-1--584-9--------1953---2--2---1--8-6--425--3-3--7---4--5---3973--------1-463--5-'
@@ -127,6 +127,14 @@ function Update-SingleMissing {
   }
   return $fnRawBoard
 } # Updatesinglemissing
+
+function Get-HiddenCell {
+  Param (
+    $fnBoardObj,
+    $fnMissingObj
+  )
+  foreach 
+}
 
 function Update-BlockRevSingle {
   Param (
