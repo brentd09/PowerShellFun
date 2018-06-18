8eb55eba557adecab4233e343a4046a8c40862ff
brent denny <brentdenny@gmail.com>
2018-06-18 23:26:51 +1000

    testing crossword word finder

--- Wordgames/Get-CrosswordWord.ps1
+++ Wordgames/Get-CrosswordWord.ps1
@@ -15,12 +15,6 @@
 #>
 [CmdletBinding()]
 Param ()
-
-
-
-# import the word list from insternet http://www-personal.umich.edu/~jlawler/wordlist
-# convert to objects that contain word,length,letter
-
 function ConvertTo-WordObject {
   Param (
     [string[]]$Words
@@ -40,4 +34,5 @@ function ConvertTo-WordObject {
 $WebContent = Invoke-WebRequest -UseBasicParsing -Uri http://www-personal.umich.edu/~jlawler/wordlist 
 [string[]]$WordList = $WebContent.Content -split "`r`n" | Where-Object {$_ -match '^[a-z]+$'}
 #$WordObjects = ConvertTo-WordObject -Words $WordList
-$WordList | Where-Object {$_.Length -eq 15 -and $_ -match 'z'}
+$WordMatch = Read-Host -Prompt 'Enter the bits of the word that you know seperated by ?'
+$wordlist | where {$_ -like $WordMatch}
