228b66bd823bba6fa592e1786196fd11ecc5c7dd
Brent Denny <brent.denny@ddls.com.au>
2018-07-18 12:54:05 +1000

    Added the sole candidate function

--- Sudoku4/Sudoku18.ps1
+++ Sudoku4/Sudoku18.ps1
@@ -4,22 +4,22 @@ Class SudokuBoardPos {
   [String]$SudokuNumber
   [int]$BoardRow
   [int]$BoardCol
-  [int]$BoardSquare
+  [int]$BoardSqr
 
   SudokuBoardPos ([String]$SudokuNumber,[int]$BoardPosition) {
     $this.SudokuNumber = $SudokuNumber
     $this.BoardPosition = $BoardPosition
     $this.BoardRow = [math]::Truncate($BoardPosition / 9)
     $this.BoardCol = $BoardPosition % 9
-    if ($BoardPosition -in @(0,1,2,9,10,11,18,19,20)) {$this.BoardSquare = 0}
-    elseif ($BoardPosition -in @(3,4,5,12,13,14,21,22,23)) {$this.BoardSquare = 1}
-    elseif ($BoardPosition -in @(6,7,8,15,16,17,24,25,26)) {$this.BoardSquare = 2}
-    elseif ($BoardPosition -in @(27,28,29,36,37,38,45,46,47)) {$this.BoardSquare = 3}
-    elseif ($BoardPosition -in @(30,31,32,39,40,41,48,49,50)) {$this.BoardSquare = 4}
-    elseif ($BoardPosition -in @(33,34,35,42,43,44,51,52,53)) {$this.BoardSquare = 5}
-    elseif ($BoardPosition -in @(54,55,56,63,64,65,72,73,74)) {$this.BoardSquare = 6}
-    elseif ($BoardPosition -in @(57,58,59,66,67,68,75,76,77)) {$this.BoardSquare = 7}
-    elseif ($BoardPosition -in @(60,61,62,69,70,71,78,79,80)) {$this.BoardSquare = 8}
+    if ($BoardPosition -in @(0,1,2,9,10,11,18,19,20)) {$this.BoardSqr = 0}
+    elseif ($BoardPosition -in @(3,4,5,12,13,14,21,22,23)) {$this.BoardSqr = 1}
+    elseif ($BoardPosition -in @(6,7,8,15,16,17,24,25,26)) {$this.BoardSqr = 2}
+    elseif ($BoardPosition -in @(27,28,29,36,37,38,45,46,47)) {$this.BoardSqr = 3}
+    elseif ($BoardPosition -in @(30,31,32,39,40,41,48,49,50)) {$this.BoardSqr = 4}
+    elseif ($BoardPosition -in @(33,34,35,42,43,44,51,52,53)) {$this.BoardSqr = 5}
+    elseif ($BoardPosition -in @(54,55,56,63,64,65,72,73,74)) {$this.BoardSqr = 6}
+    elseif ($BoardPosition -in @(57,58,59,66,67,68,75,76,77)) {$this.BoardSqr = 7}
+    elseif ($BoardPosition -in @(60,61,62,69,70,71,78,79,80)) {$this.BoardSqr = 8}
   }
 }
 
@@ -32,7 +32,7 @@ Class SudokuBoardPos {
 # Extreme    '--9748---7---------2-1-9-----7---24--64-1-59--98---3-----8-3-2---------6---2759--'
 # Extreme    '--5--7--4-6--5--9-4--9--2--2--5--1---7--2--4---8--3--2--7--1--3-5--6--1-6--8--4--'
 
-$RawBoard = '-1--584-9--------1953---2--2---1--8-6--425--3-3--7---4--5---3973--------1-463--5-'
+$RawBoard = '-2-------17---9--4---1367--431---2------8------8---163--3624---2--8---49-------3-'
 
 function Create-BoardObj {
   Param(
@@ -49,7 +49,6 @@ function Show-Board {
   param (
     $BoardObj
   )
-  #Clear-Host
   $Coords = New-Object -TypeName System.Management.Automation.Host.Coordinates
   $host.UI.RawUI.CursorPosition = $Coords
   Write-Host
@@ -82,13 +81,43 @@ function Complete-SoleCandidate {
   $AllNumbers = @('1','2','3','4','5','6','7','8','9')
   foreach ($Pos in (0..80)){
     if ($BoardObj[$Pos].SudokuNumber -eq '-') {
-      $AllRelated = $BoardObj | Where-Object {$_.BoardCol -eq $BoardObj[$Pos].BoardCol -or $_.BoardRow -eq $BoardObj[$Pos].BoardRow -or $_.BoardSqr -eq $BoardObj[$Pos].BoardSqr}
-      $AllRelated = $AllRelated | Where-Object {$_.SudokuNumber -ne '-'} | Select-Object -Unique
-      $WhatsMissing = (Compare-Object -ReferenceObject $AllRelated -DifferenceObject $AllNumbers).InputObject
-      if (($WhatsMissing | Measure-Object).Count -eq 1) {}#found unique solution
+      $AllRelatedObj = $BoardObj | Where-Object {
+        $_.BoardCol -eq $BoardObj[$Pos].BoardCol -or
+        $_.BoardRow -eq $BoardObj[$Pos].BoardRow -or
+        $_.BoardSqr -eq $BoardObj[$Pos].BoardSqr
+      }
+      $AllRelatedNums = $AllRelatedObj.SudokuNumber |
+        Where-Object {$_ -ne '-'} |
+        Select-Object -Unique |
+        Sort-Object
+      $WhatsMissing = (Compare-Object -ReferenceObject $AllRelatedNums -DifferenceObject $AllNumbers).InputObject
+      if (($WhatsMissing | Measure-Object).Count -eq 1) {
+        $BoardObj[$Pos].SudokuNumber = $WhatsMissing
+      }#found unique solution
     }
+  }
+  return $BoardObj
+}
+function Complete-UniqueCandidate {
+  Param (
+    $BoardObj
+  )
+  foreach ($Pos in (0..80)){
+    
   }
 }
 
+
+########### MAIN CODE ############
+Clear-Host
 $BoardObj = Create-BoardObj -RawBrd $RawBoard
-Show-Board -fnBoardObj $BoardObj
+Show-Board -BoardObj $BoardObj
+do {
+  $InitBlankCount = ($BoardObj | Where-Object {$_.SudokuNumber -eq '-'} | Measure-Object ).Count
+  $BoardObj = Complete-SoleCandidate -BoardObj $BoardObj
+  $FinalBlankCount = ($BoardObj | Where-Object {$_.SudokuNumber -eq '-'} | Measure-Object ).Count
+  if ($FinalBlankCount -eq $InitBlankCount) {"more adv soln required" ; Read-Host}
+  Show-Board -BoardObj $BoardObj
+
+  Start-Sleep -Milliseconds 500
+} while ($BoardObj.SudokuNumber -contains '-')
