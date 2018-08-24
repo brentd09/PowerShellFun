bbbee15152131917c3784deb4be8f4c39f86263a
brent denny <brentdenny@gmail.com>
2018-08-25 03:57:26 +1000

    Added a play again loop

--- Play21/Play-BlackJack-v2.ps1
+++ Play21/Play-BlackJack-v2.ps1
@@ -73,95 +73,97 @@ function Check-WhoWon {
 #-------------- 
 # MAIN CODE
 
-
-# init variables 
-$OrderedCards = @()
-$DealerHand = @()
-$PlayerHand = @()
-$OrderedCards = Build-OrderedDeck
-$InitShuffle = $OrderedCards | Sort-Object{Get-Random} # this will shuffle a list of objects, e.g. PlayingCards
-$DealersTurn = $false ; $PlayersTurn = $true
-$NextCardPosition = 0
-$DealerTurnNum = 0
-# iv
-
-# initial hand deal
-$DealerHand = @(0,2)
-$PlayerHand = @(1,3)
-$NextCardPosition = 4
-# ihd
-
 do {
-  Clear-Host
-  if ($PlayersTurn) {$Title = "Players "}
-  elseif ($DealersTurn) {$Title = "Dealers "}
-  Write-Host -ForegroundColor black -BackgroundColor Red "$Title Turn"
-  Write-Host ''
-  # Display the Dealers Hand
-  Write-Host -ForegroundColor Yellow  "Dealer Hand"
-  $DealerStatus = Check-HandScore -EntireHand $InitShuffle[$DealerHand] 
-  foreach ($DealerCard in $DealerHand) {
-    if (-not $DealersTurn -and $DealerCard -eq $DealerHand[0]) {Display-Card -DeckShuffled $InitShuffle -WhichCard $DealerCard -HideCard}
-    else {Display-Card -DeckShuffled $InitShuffle -WhichCard $DealerCard}
-  }
-  Write-Host ''
-  if ($DealerStatus.Busted) { 
-    Write-Host -ForegroundColor Red "BUSTED"
-    $DealersTurn = $false
-  }
-  elseif ($DealersTurn) {
-    Write-Host -ForegroundColor Cyan $($DealerStatus.Score)
-    $DealerTurnNum++
-  }
-  else {Write-Host -ForegroundColor Cyan "Waiting For Player"}
-  # Display the Players Hand
-  Write-Host -ForegroundColor Green  "`nPlayer Hand"
-  $PlayerStatus = Check-HandScore -EntireHand $InitShuffle[$PlayerHand] 
-  foreach ($PlayerCard in $PlayerHand) {
-    Display-Card -DeckShuffled $InitShuffle -WhichCard $PlayerCard
-  }
-  Write-Host ''
-  if ($PlayerStatus.Busted) { 
-    Write-Host -ForegroundColor Red "BUSTED"
-    $PlayersTurn = $false
-    $DealersTurn = $true
-  }
-  else {Write-Host -ForegroundColor Cyan $($PlayerStatus.Score)}
-  Write-Host ''
-  # Get Player to choose to HIT or STAND
-  if ($PlayersTurn) {
-    $Question = Read-Host -Prompt "[H]it or [S]tand"
-    if ($Question -like "h*") {
-      $PlayerHand += $NextCardPosition
-      $NextCardPosition++
-    }  
-    else {
+  # init variables 
+  $OrderedCards = @()
+  $DealerHand = @()
+  $PlayerHand = @()
+  $OrderedCards = Build-OrderedDeck
+  $InitShuffle = $OrderedCards | Sort-Object{Get-Random} # this will shuffle a list of objects, e.g. PlayingCards
+  $DealersTurn = $false ; $PlayersTurn = $true
+  $NextCardPosition = 0
+  $DealerTurnNum = 0
+  # iv
+  
+  # initial hand deal
+  $DealerHand = @(0,2)
+  $PlayerHand = @(1,3)
+  $NextCardPosition = 4
+  # ihd
+  
+  do {
+    Clear-Host
+    if ($PlayersTurn) {$Title = "Players "}
+    elseif ($DealersTurn) {$Title = "Dealers "}
+    Write-Host -ForegroundColor black -BackgroundColor Red "$Title Turn"
+    Write-Host ''
+    # Display the Dealers Hand
+    Write-Host -ForegroundColor Yellow  "Dealer Hand"
+    $DealerStatus = Check-HandScore -EntireHand $InitShuffle[$DealerHand] 
+    foreach ($DealerCard in $DealerHand) {
+      if (-not $DealersTurn -and $DealerCard -eq $DealerHand[0]) {Display-Card -DeckShuffled $InitShuffle -WhichCard $DealerCard -HideCard}
+      else {Display-Card -DeckShuffled $InitShuffle -WhichCard $DealerCard}
+    }
+    Write-Host ''
+    if ($DealerStatus.Busted) { 
+      Write-Host -ForegroundColor Red "BUSTED"
+      $DealersTurn = $false
+    }
+    elseif ($DealersTurn) {
+      Write-Host -ForegroundColor Cyan $($DealerStatus.Score)
+      $DealerTurnNum++
+    }
+    else {Write-Host -ForegroundColor Cyan "Waiting For Player"}
+    # Display the Players Hand
+    Write-Host -ForegroundColor Green  "`nPlayer Hand"
+    $PlayerStatus = Check-HandScore -EntireHand $InitShuffle[$PlayerHand] 
+    foreach ($PlayerCard in $PlayerHand) {
+      Display-Card -DeckShuffled $InitShuffle -WhichCard $PlayerCard
+    }
+    Write-Host ''
+    if ($PlayerStatus.Busted) { 
+      Write-Host -ForegroundColor Red "BUSTED"
       $PlayersTurn = $false
       $DealersTurn = $true
-      $DealerTurnNum = 0
     }
-  }
-  # Dealer needs to find out if cards are needed to win
-  if ($DealersTurn -and $PlayerStatus.Busted -eq $false) {
-    if ($DealerStatus.score -le 17) {
-      $DealerHand += $NextCardPosition
-      $NextCardPosition++
-    } 
-    elseif ($DealerStatus.score -lt $PlayerStatus.Score) {
-      $DealerHand += $NextCardPosition
-      $NextCardPosition++
+    else {Write-Host -ForegroundColor Cyan $($PlayerStatus.Score)}
+    Write-Host ''
+    # Get Player to choose to HIT or STAND
+    if ($PlayersTurn) {
+      $Question = Read-Host -Prompt "[H]it or [S]tand"
+      if ($Question -like "h*") {
+        $PlayerHand += $NextCardPosition
+        $NextCardPosition++
+      }  
+      else {
+        $PlayersTurn = $false
+        $DealersTurn = $true
+        $DealerTurnNum = 0
+      }
     }
-    elseif ($DealerTurnNum -gt 0) {$DealersTurn = $false}
-  }
-  if ($DealersTurn -and $PlayerStatus.Busted -eq $true -and $DealerTurnNum -gt 0) {
-    $DealersTurn = $false
-  }
-
-} until ($DealersTurn -eq $false -and $PlayersTurn -eq $false)
-# Determine the winner, if there is one
-$Winner = Check-WhoWon -DealerInfo $DealerStatus -PlayerInfo $PlayerStatus
-Write-Host -NoNewline  "Winner is:"
-if ($Winner -eq 'Player') {$WinColor = 'Green'}
-if ($Winner -eq 'Dealer') {$WinColor = 'Yellow'}
-
-write-host -foregroundcolor $WinColor "$Winner"
+    # Dealer needs to find out if cards are needed to win
+    if ($DealersTurn -and $PlayerStatus.Busted -eq $false) {
+      if ($DealerStatus.score -le 17) {
+        $DealerHand += $NextCardPosition
+        $NextCardPosition++
+      } 
+      elseif ($DealerStatus.score -lt $PlayerStatus.Score) {
+        $DealerHand += $NextCardPosition
+        $NextCardPosition++
+      }
+      elseif ($DealerTurnNum -gt 0) {$DealersTurn = $false}
+    }
+    if ($DealersTurn -and $PlayerStatus.Busted -eq $true -and $DealerTurnNum -gt 0) {
+      $DealersTurn = $false
+    }
+  
+  } until ($DealersTurn -eq $false -and $PlayersTurn -eq $false)
+  # Determine the winner, if there is one
+  $Winner = Check-WhoWon -DealerInfo $DealerStatus -PlayerInfo $PlayerStatus
+  Write-Host -NoNewline  "Winner is:"
+  if ($Winner -eq 'Player') {$WinColor = 'Green'}
+  if ($Winner -eq 'Dealer') {$WinColor = 'Yellow'}
+  
+  write-host -foregroundcolor $WinColor "$Winner"
+  $PlayAgain = Read-Host -Prompt "Do you want to play again Y/N"
+} until ($PlayAgain -notlike "y*")  
