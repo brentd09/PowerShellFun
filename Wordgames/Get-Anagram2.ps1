87ab5c99e065b2deb538d1d1b48797392b1662d4
brent denny <brentdenny@gmail.com>
2018-08-30 11:16:56 +1000

    anagram2 added ability to group and pick sizes of words

--- Wordgames/Get-Anagram2.ps1
+++ Wordgames/Get-Anagram2.ps1
@@ -2,11 +2,12 @@
 Param (
   [Parameter(Mandatory=$true)]
   [string]$Letters,
-  [switch]$Group
+  [switch]$Group,
+  [int[]]$WordLength = 0
 )
 function Get-Words {
   Param ($LettersFn)
-  $Rawweb = Invoke-WebRequest -uri "https://new.wordsmith.org/anagram/anagram.cgi?anagram=$LettersFn&language=english&t=500&d=&include=&exclude=&n=4&m=&a=n&l=y&q=y&k=0" -UseBasicParsing -Method get
+  $Rawweb = Invoke-WebRequest -uri "https://new.wordsmith.org/anagram/anagram.cgi?anagram=$LettersFn&language=english&t=500&d=&include=&exclude=&n=3&m=&a=n&l=y&q=y&k=0" -UseBasicParsing -Method get
   $WebcontentArray = $Rawweb -split "`n"
   $wordList = $WebcontentArray | Where-Object {$_ -match "^<br>\s*\d+\.\s+[a-z]+"}
   $Words = $wordList -replace "^<br>\s*\d+\.\s+([a-z]+)",'$1' 
@@ -14,10 +15,20 @@ function Get-Words {
     New-Object -TypeName psobject -Property @{Words = $Word;CharLength = $Word.Length}
   }
 }
-
-If ($Group -eq $true) {
-  Get-Words -LettersFn $Letters | Format-Wide -GroupBy CharLength -Property Words -AutoSize
+if ($WordLength -contains 0) {
+  If ($Group -eq $true) {
+    Get-Words -LettersFn $Letters | Format-Wide -GroupBy CharLength -Property Words -AutoSize
+  }
+  else {
+    Get-Words -LettersFn $Letters
+  }
 }
 else {
-  Get-Words -LettersFn $Letters
+  If ($Group -eq $true) {
+    Get-Words -LettersFn $Letters | Where-Object {$_.CharLength -in $WordLength}| Format-Wide -GroupBy CharLength -Property Words -AutoSize
+  }
+  else {
+    Get-Words -LettersFn $Letters| Where-Object {$_.CharLength -in $WordLength}
+  }
+
 }
