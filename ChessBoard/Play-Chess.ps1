d9bda6bff401035f161be43ec714bb62f79ecaba
Brent Denny <brent.denny@ddls.com.au>
2018-09-04 16:38:06 +1000

    chess board not going so well

---
+++ ChessBoard/Play-Chess.ps1
@@ -0,0 +1,48 @@
+$tlc = [char]9484
+$hb  = [char]9480
+$tt  = [char]9516
+$trc = [char]9488
+$lt  = [char]9500
+$rt  = [char]9508
+$blc = [char]9492
+$bt  = [char]9524
+$brc = [char]9496
+$mc  = [char]9532
+$vb  = [char]9474
+$BK = [char]9812
+$BQ = [char]9813
+$BR = [char]9814
+$BB = [char]9815
+$BK = [char]9816
+$BP = [char]9817
+$WK = [char]9818
+$WQ = [char]9819
+$WR = [char]9820
+$WB = [char]9821
+$WK = [char]9822
+$WP = [char]9823
+
+write-host -BackgroundColor DarkGray -NoNewline $tlc$hb$hb$hb
+Write-Host $tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$trc
+Write-Host -BackgroundColor darkgray -NoNewline "$vb $br "
+write-host "$vb $bk $vb $bb $vb $bq $vb $bk $vb $bb $vb $bk $vb $br $vb"
+write-host -BackgroundColor darkgray -NoNewline $lt$hb$hb$hb
+Write-Host $mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
+Write-Host "$vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb"
+write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
+Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
+write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
+Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
+write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
+Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
+write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
+Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
+write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
+Write-Host "$vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb"
+write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
+Write-Host "$vb $wr $vb $wk $vb $wb $vb $wq $vb $wk $vb $wb $vb $wk $vb $wr $vb"
+write-host $blc$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$brc
+
+
+
+#9812..9823 | foreach { $char =[char]$_ ;write-host -NoNewline "$char : "; $_; ""}
