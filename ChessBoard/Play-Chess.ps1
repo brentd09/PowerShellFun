a7b44ffd520fde99ba9f9f926e7369e49ba7779e
Brent Denny <brent.denny@ddls.com.au>
2018-09-05 11:59:40 +1000

    more chess

--- ChessBoard/Play-Chess.ps1
+++ ChessBoard/Play-Chess.ps1
@@ -13,35 +13,34 @@ $BK = [char]9812
 $BQ = [char]9813
 $BR = [char]9814
 $BB = [char]9815
-$BK = [char]9816
+$BN = [char]9816
 $BP = [char]9817
 $WK = [char]9818
 $WQ = [char]9819
 $WR = [char]9820
 $WB = [char]9821
-$WK = [char]9822
+$WN = [char]9822
 $WP = [char]9823
 
-write-host -BackgroundColor DarkGray -NoNewline $tlc$hb$hb$hb
-Write-Host $tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$tt$hb$hb$hb$trc
-Write-Host -BackgroundColor darkgray -NoNewline "$vb $br "
-write-host "$vb $bk $vb $bb $vb $bq $vb $bk $vb $bb $vb $bk $vb $br $vb"
-write-host -BackgroundColor darkgray -NoNewline $lt$hb$hb$hb
-Write-Host $mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
-Write-Host "$vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb $bp $vb"
-write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
-Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
-write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
-Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
-write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
-Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
-write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
-Write-Host "$vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb   $vb"
-write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
-Write-Host "$vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb $wp $vb"
-write-host $lt$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$mc$hb$hb$hb$rt
-Write-Host "$vb $wr $vb $wk $vb $wb $vb $wq $vb $wk $vb $wb $vb $wk $vb $wr $vb"
-write-host $blc$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$bt$hb$hb$hb$brc
+Write-Host "                 PGN Player"
+
+write-host $tlc$hb$hb$hb$hb$tt$hb$hb$hb$hb$tt$hb$hb$hb$hb$tt$hb$hb$hb$hb$tt$hb$hb$hb$hb$tt$hb$hb$hb$hb$tt$hb$hb$hb$hb$tt$hb$hb$hb$hb$trc
+Write-Host "$vb $br  $vb $bn  $vb $bb  $vb $bq  $vb $bk  $vb $bb  $vb $bn  $vb $br  $vb"
+write-host $lt$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$rt
+Write-Host "$vb $bp  $vb $bp  $vb $bp  $vb $bp  $vb $bp  $vb $bp  $vb $bp  $vb $bp  $vb"
+write-host $lt$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$rt
+Write-Host "$vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb"
+write-host $lt$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$rt
+Write-Host "$vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb"
+write-host $lt$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$rt
+Write-Host "$vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb"
+write-host $lt$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$rt
+Write-Host "$vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb    $vb"
+write-host $lt$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$rt
+Write-Host "$vb $wp  $vb $wp  $vb $wp  $vb $wp  $vb $wp  $vb $wp  $vb $wp  $vb $wp  $vb"
+write-host $lt$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$mc$hb$hb$hb$hb$rt
+Write-Host "$vb $wr  $vb $wn  $vb $wb  $vb $wq  $vb $wk  $vb $wb  $vb $wn  $vb $wr  $vb"
+write-host $blc$hb$hb$hb$hb$bt$hb$hb$hb$hb$bt$hb$hb$hb$hb$bt$hb$hb$hb$hb$bt$hb$hb$hb$hb$bt$hb$hb$hb$hb$bt$hb$hb$hb$hb$bt$hb$hb$hb$hb$brc
 
 
 
