22a570dfae43644305c6973f7ecd7128a4bb5c5d
brent denny <brentdenny@gmail.com>
2018-09-24 23:08:26 +1000

    better guessing logic for computer

--- BattleShip/Play-BattleShipv2.ps1
+++ BattleShip/Play-BattleShipv2.ps1
@@ -211,8 +211,8 @@ function Guess-Move {
   if ($ComputersTurn -eq $true) {
     $Likely = Find-LikelyHits -Grid $OpponentGrid.psobject.copy()
     if ($Likely.Random -eq $true) {
-      $GuessArray = @(0,2,4,6,8,11,13,15,17,19,20,22,24,26,28,31,33,35,37,39,40,42,44,46,48,51,53,55,57,59,60,62,64,66,68,71,73,75,77,79,80,82,84,86,88,91,93,95,97,99)
-      #$GuessArray = @(0,3,6,9,11,14,17,22,25,28,30,33,36,39,41,44,47,52,55,58,60,63,66,69,71,74,77,82,85,88,90,93,96,99)
+      #$GuessArray = @(0,2,4,6,8,11,13,15,17,19,20,22,24,26,28,31,33,35,37,39,40,42,44,46,48,51,53,55,57,59,60,62,64,66,68,71,73,75,77,79,80,82,84,86,88,91,93,95,97,99)
+      $GuessArray = @(0,3,6,9,11,14,17,22,25,28,30,33,36,39,41,44,47,52,55,58,60,63,66,69,71,74,77,82,85,88,90,93,96,99)
       $PosRemaining = ($OpponentGrid | where {$_.Pegchosen -eq $false -and $_.Pos -in $GuessArray} ).Pos
       if ($PosRemaining.count -eq 0) {$PosRemaining = ($OpponentGrid | Where-Object {$_.PegChosen -eq $false}).Pos }
       $GuessPos = $PosRemaining | get-random
