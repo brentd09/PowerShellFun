ae656a0577f23cf90c14166d2f2fd02954d7f3d2
Brent Denny <brent.denny@ddls.com.au>
2018-10-25 08:56:28 +1100

    added the tiebreaking rules in the comments

--- TexasHoldem/Play-TexasHoldem.ps1
+++ TexasHoldem/Play-TexasHoldem.ps1
@@ -207,4 +207,47 @@ Three of a Kind
 Two Pairs
 Single Pair
 High Card
+
+Tie Breaker Rules of Poker Cash Game - Poker Rules
+-------------------------------------------------- 
+Know the detailed Tie Breaker Rules of Poker Cash Game.
+------------------------------------------------------
+RANKING	RANK-CARD(S)	KICKER-CARD(S)	TIE BREAKERS
+ROYAL FLUSH	Royal Flush Cards	NA	
+  An ace-high Straight flush is called Royal flush. 
+  A Royal Flush is the highest hand in poker. 
+  Between two Royal flushes, there can be no tie breaker. If two players have Royal Flushes, they split the pot. 
+  The odds of this happening though are very rare and almost impossible in texas holdem because board requires 
+  three cards of one suit for anyone to have a flush in that suit.
+STRAIGHT FLUSH	Top Card	NA	
+  Straight flushes come in varying strengths from five high to a king high. A King High Straight Flush loses only to a Royal. 
+  If more than one player has a Straight Flush, the winner is the player with the highest card used in the Straight. 
+  A queen high Straight Flush beats a jack high and a jack high beats a ten high and so on. 
+  The suit never comes into play i.e. a seven High Straight Flush of Diamonds will split the pot with a seven high Straight Flush of hearts.
+FOUR OF A KIND	Four of a Kind Card	Remaining 1	
+  This one is simple. Four Aces beats any other four of a kind, four Kings beats four queens or less and so on. The only tricky part of a tie 
+  breaker with four of a kind is when the four falls on the table in a game of Texas Holdem and is therefore shared between two (or more) players. 
+  A kicker can be used, however if the fifth community card is higher than any card held by any player still in the hand, then the 
+  hand is considered a tie and the pot is split.
+FULL HOUSE	Trips & Pair Card	NA	
+  When two or more players have full houses, we look first at the strength of the three of a kind to determine the winner. 
+  For example, Aces full of deuces (AAA22) beats Kings full of Jacks (KKKJJ). If there are three of a kind on the table (community cards) in a 
+  Texas Holdem game that are used by two or more players to make a full house, then we would look at the strength of the pair to determine a winner.
+FLUSH	Flush Cards	NA	A flush is any hand with five cards of the same suit. 
+  If two or more players hold a flush, the flush with the highest card wins. If more than one player has the same strength high card, 
+  then the strength of the second highest card held wins. This continues through the five highest cards in the player's hands.
+STRAIGHT	Top Card	NA	A straight is any five cards in sequence, but not necessarily of the same suit. 
+  If more than one player has a straight, the straight ending in the card wins. If both straights end in a card of the same strength, the hand is tied.
+THREE OF A KIND	Trips Card	Remaining 2	
+  If more than one player holds three of a kind, then the higher value of the cards used to make the three of kind determines the winner. 
+  If two or more players have the same three of a kind, then a fourth card (and a fifth if necessary) can be used as kickers to determine the winner.
+TWO PAIR	1st & 2nd Pair Card	Remaining 1	
+  The highest pair is used to determine the winner. If two or more players have the same highest pair, then the highest of the second pair determines the winner. 
+  If both players hold identical two pairs, fifth card is used to break the tie.
+ONE PAIR	Pair Card	Remaining 3	
+  If two or more players hold a single pair, then highest pair wins. If the pairs are of the same value, the highest kicker card determines the winner. 
+  A second and even third kicker can be used if necessary.
+HIGH CARD	Top Card	Remaining 4	
+  When no player has even a pair, then the highest card wins. When both players have identical high cards, the next highest card wins, 
+  and so on until five cards have been used. In the unusual circumstance that two players hold the identical five cards, the pot would be split.
 #>
