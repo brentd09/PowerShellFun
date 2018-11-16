5536e2f8f124872c418e127c0394f96ea8e4a6c1
Brent Denny <brent.denny@ddls.com.au>
2018-11-17 10:51:25 +1100

    Added a new anagram solver Discover-Anagrams

---
+++ Wordgames/Discover-Anagrams.ps1
@@ -0,0 +1,31 @@
+<#
+.SYNOPSIS
+  Discovers a word from the letters given
+.DESCRIPTION
+  Long description
+.EXAMPLE
+  PS C:\> <example usage>
+  Explanation of what the example does
+.INPUTS
+  Inputs (if any)
+.OUTPUTS
+  Output (if any)
+.NOTES
+  General notes
+#>
+[CmdletBinding()]
+Param()
+$webDictSite = Invoke-WebRequest -Uri http://www.mieliestronk.com/corncob_lowercase.txt 
+[string[]]$Words = ($webDictSite.Content -split "`n" | Where-Object {$_.length -ge 3}).trim() 
+$MatchWord = ''
+While ($MatchWord -ne "q") {
+  $MatchWord = Read-Host -Prompt "Enter the word with - for unknown letters, Q - Quit"
+  if ($MatchWord -eq 'q') {continue}
+  [string]$RegExMW = ($MatchWord -replace "[^a-z]",'.').trim()
+  Write-Host '-------------------------'
+  Write-Host $MatchWord
+  Write-Host '-------------------------'
+  [string[]]$Result = $Words -match "^$RegExMW$" 
+  $Result |select-object -property   @{Name="Word"; Expression = {$_}} | Format-Wide -AutoSize
+  Write-Host '-------------------------'
+}
