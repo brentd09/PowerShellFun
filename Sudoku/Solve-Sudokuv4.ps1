723532ff8077edd278458b2d42b6c225b83466ec
Brent Denny <brent.denny@ddls.com.au>
2019-05-20 21:47:57 +1000

    sudoku4

--- Sudoku/Solve-Sudokuv4.ps1
+++ Sudoku/Solve-Sudokuv4.ps1
@@ -8,29 +8,29 @@
   Unique candidate, sole candidate, hidden candidate, hidden pair, naked pair
   Soon it will be able to solve for X-Wing and SwordFish as well.
 .EXAMPLE
-  Solve-Sudoku -Puzzle '1----3-----6--5----291--6-361-5-23---3--6--9---84-1-267-5--943----7--8-----3----9'
+  Solve-Sudoku -PuzzleString '1----3-----6--5----291--6-361-5-23---3--6--9---84-1-267-5--943----7--8-----3----9'
   This represents the Puzzle as a string to the script
 .Notes  
   General notes
   Created By: Brent Denny
           On: 05-Mar-2019
   Puzzles        
-    $Puzzle = '7-542--6-68-1--24--4-76--18-91--2-7482--576--3---1482-158--6--9--25-91-6--684-7-2' Easy
-    $Puzzle = '-9--2-5----4--5-1--6-----93--18---6----9----2-8--72---5----1-7----3--9-1-13------' Medium
-    $Puzzle = '-9--2-5----4--5-1--6-----93--18---6----9----2-8--72---5----1-7----3--9-1--3------' Difficult
-    $Puzzle = '--97486--7---------2-1-987---7---24--64-1-59--98---3-----8-3-2---------6---2759--' Difficult
-    $Puzzle = '-714---------17--59------4-5-8-6341--3--------9-----28-----4-6--6--89--1----3--5-' Difficult
-    $Puzzle = '-2-6------562-----1------28----2-4-9-914-873-2-8-9----71------3-----217------5-6-' Difficult
-    $Puzzle = '89-2-3-------------3658---41-8-3--6-----------2--7-3-57---9412-------------8-2-59' Extreme
-    $Puzzle = '--9748---7---------2-1-9-----7---24--64-1-59--98---3-----8-3-2---------6---2759--' Impossible
-    $Puzzle = '-714---------17--59------4-5-8-634---3--------9-----28-----4-6--6--89--1----3--5-' Impossible
-    $Puzzle = '----15-74----3-8---87---5-1-23--4----1--7--2----2--79-8-6---24---1-2----23-64----' impossible
-    $Puzzle = '--7---28---4-25---28---46---9---6---3-------2---1---9---62---75---57-4---78---3--' impossible 
-    $Puzzle = '1-----569492-561-8-561-924---964-8-1-64-1----218-356-4-4-5---1-9-5-614-2621-----5' impossible (xwing)
+    $PuzzleString = '7-542--6-68-1--24--4-76--18-91--2-7482--576--3---1482-158--6--9--25-91-6--684-7-2' Easy
+    $PuzzleString = '-9--2-5----4--5-1--6-----93--18---6----9----2-8--72---5----1-7----3--9-1-13------' Medium
+    $PuzzleString = '-9--2-5----4--5-1--6-----93--18---6----9----2-8--72---5----1-7----3--9-1--3------' Difficult
+    $PuzzleString = '--97486--7---------2-1-987---7---24--64-1-59--98---3-----8-3-2---------6---2759--' Difficult
+    $PuzzleString = '-714---------17--59------4-5-8-6341--3--------9-----28-----4-6--6--89--1----3--5-' Difficult
+    $PuzzleString = '-2-6------562-----1------28----2-4-9-914-873-2-8-9----71------3-----217------5-6-' Difficult
+    $PuzzleString = '89-2-3-------------3658---41-8-3--6-----------2--7-3-57---9412-------------8-2-59' Extreme
+    $PuzzleString = '--9748---7---------2-1-9-----7---24--64-1-59--98---3-----8-3-2---------6---2759--' Impossible
+    $PuzzleString = '-714---------17--59------4-5-8-634---3--------9-----28-----4-6--6--89--1----3--5-' Impossible
+    $PuzzleString = '----15-74----3-8---87---5-1-23--4----1--7--2----2--79-8-6---24---1-2----23-64----' impossible
+    $PuzzleString = '--7---28---4-25---28---46---9---6---3-------2---1---9---62---75---57-4---78---3--' impossible 
+    $PuzzleString = '1-----569492-561-8-561-924---964-8-1-64-1----218-356-4-4-5---1-9-5-614-2621-----5' impossible (xwing)
 #>
 [CmdletBinding()]
 Param (
-  $Puzzle = '7-542--6-68-1--24--4-76--18-91--2-7482--576--3---1482-158--6--9--25-91-6--684-7-2'
+  $PuzzleString = '-9--2-5----4--5-1--6-----93--18---6----9----2-8--72---5----1-7----3--9-1-13------'
 )
 class SudokuCell {
   [string]$Val
@@ -65,10 +65,14 @@ class SudokuCell {
     }
   } # New Object method
 
-  [void]AssignValue ([string]$Value) {
+  [void]AssignValue ([string[]]$Value) {
     $this.Val = $Value
     $this.PossibleValues = $Value
     $this.NotPossibleValues = $this.NotPossibleValues.Remove($Value)
+  }
+
+  [void]AssignPossible ([string[]]$Possibles) {
+    $this.PossibleValues = $Possibles
   }
 
   [void]AssignNotPossible ([string[]]$Value) {
@@ -82,3 +86,82 @@ function New-Board {
     [SudokuCell]::New($Boardstring.Substring($Pos,1),$Pos)
   }
 }
+
+function Get-ArrayDifference {
+  Param (
+    [string[]]$StringArray,
+    [string[]]$ReferenceArray = @('1','2','3','4','5','6','7','8','9')
+  )
+  [string[]]$ElementsNotInRef = (Compare-Object -ReferenceObject $ReferenceArray -DifferenceObject $StringArray).InputObject
+  $Hash = [ordered]@{
+    InputArray = $StringArray
+    ReferenceArray = $ReferenceArray
+    ElementsMissing = $ElementsNotInRef
+    MissingCount = $ElementsNotInRef.Count
+  }
+  New-Object -TypeName psobject -Property $Hash
+}
+
+function Get-SoleCandidate {
+  Param (
+    [SudokuCell[]]$GameBoard
+  )
+  foreach ($Pos in (0..80)) {
+    if ($GameBoard[$Pos].Val -notmatch '[1-9]') {
+      $Row = $GameBoard[$Pos].Row
+      $Col = $GameBoard[$Pos].Col
+      $Box = $GameBoard[$Pos].Box
+  
+      $RelatedNumbers = ($GameBoard | Where-Object {$_.Row -eq $Row -or $_.Col -eq $Col -or $_.Box -eq $Box}).Val | Where-Object {$_ -match '[1-9]'}
+      $UniqueRelatedNumbers = $RelatedNumbers | Select-Object -Unique | Sort-Object
+      $MissingNumbers = Get-ArrayDifference -StringArray $UniqueRelatedNumbers
+      $GameBoard[$Pos].AssignPossible($MissingNumbers.ElementsMissing)
+      if ($MissingNumbers.MissingCount -eq 1) {$GameBoard[$Pos].AssignValue($MissingNumbers.ElementsMissing)}
+    }
+  }
+}
+function Get-UniqueCandidate {
+  Param ([SudokuCell[]]$GameBoard)
+  
+}
+
+
+
+function Show-Board {
+  Param ([SudokuCell[]]$GameBoard)
+  $Coords = New-Object -TypeName System.Management.Automation.Host.Coordinates
+  $host.UI.RawUI.CursorPosition = $Coords
+  $FGColor = 'Yellow'
+  Write-Host -ForegroundColor Green "     Solve Sudoku`n"
+  foreach ($PosCol in (0..8)) {
+    if ($PosCol -eq 2 -or $PosCol -eq 5) {$HBdr = "`n------+-------+------"}
+    else {$HBdr = ''}
+    foreach ($PosRow in (0..8)) {
+      if ($PosRow -eq 2 -or $PosRow -eq 5) {$Bdr = ' | '}
+      else {$Bdr = ' '}
+      Write-Host -NoNewline $GameBoard[($PosRow+($PosCol*9))].Val
+      Write-Host -NoNewline -ForegroundColor $FGColor "$Bdr"
+    }
+    Write-Host -ForegroundColor $FGColor $HBdr
+  }
+  Write-Host
+}
+
+# ##################################################
+# Main Code
+Clear-Host
+[SudokuCell[]]$SudokuGameBoard = New-Board -BoardString $PuzzleString
+Show-Board -GameBoard $SudokuGameBoard
+$PreNumberToGuess = 81
+do {
+
+  Get-SoleCandidate -GameBoard $SudokuGameBoard
+  [int]$NumberStillToGuess = ($SudokuGameBoard | Where-Object {$_.Val -notmatch '[1-9]'}).Count
+  if ($NumberStillToGuess -eq $PreNumberToGuess) {
+
+  }
+  Start-Sleep -Seconds 2
+  #Show-Board -GameBoard $SudokuGameBoard
+  $SudokuGameBoard | ft
+  [int]$NumberStillToGuess = ($SudokuGameBoard | Where-Object {$_.Val -notmatch '[1-9]'}).Count
+} until ($NumberStillToGuess -eq 0)
