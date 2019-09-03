bf53d6313e798ccd2d79fe2c1d7006dec204368c
Brent Denny <brent.denny@ddls.com.au>
2019-09-03 11:25:35 +1000

    ttt new

--- TTT/Play-TTTv2.ps1
+++ TTT/Play-TTTv2.ps1
@@ -2,12 +2,10 @@ Class TTTBoardCell {
   [int]$Pos
   [int]$Col
   [int]$Row
-  [Int]$Diag # Diag 0 = 0-5-8 , Diag 1 = 2,5,6
+  [Int]$Diag # Diag = 0 => 0-5-8 , Diag = 1 => 2,5,6
   [string]$Content
 
   TTTBoardCell($Position) {
-    $this.BackDiag = $false
-    $this.FwdDiag  = $false
     $ColNum = $Position % 3
     $RowNum = [math]::Truncate($Position / 3)
     $this.Pos = $Position
@@ -26,41 +24,96 @@ Class TTTBoardCell {
     else {return $false}
   }
   
-  [psobject]RowThreats([TTTBoardCell[]]$Board,[string]$TurnLetter) {
+  [psobject[]]RowThreats([TTTBoardCell[]]$Board,[string]$TurnLetter) {
+    [psobject[]]$Result = @()
     $OppositeLetter = @('X','O') | Where-Object {$_ -ne $TurnLetter}
     foreach ($Index in (0..2)) {
       $RowObjects = $Board | Where-Object {$_.Row -eq $Index}
       $NumberOfBlanks = ($RowObjects | Group-Object | Where-Object {$_.Content -eq '-'}).count
+      $ThreatRow = @{
+        ThreatReal = $false
+        RowIndex = $Index
+        EmptyPos = ($RowObjects | Where-Object {$_.Content -eq '-' }).Pos
+      } 
       if ($NumberOfBlanks -eq 1) {
         $NumberOfOpposite = ($RowObjects | Group-Object | Where-Object {$_.Content -eq $OppositeLetter[0]}).count
         if ($NumberOfOpposite -eq 2) {
-          $ThreatRowProp = @{
+          [hashtable]$ThreatRow = @{
             ThreatReal = $true
-            RowIndexUnderThreat = $Index
+            RowIndex = $Index
             EmptyPos = ($RowObjects | Where-Object {$_.Content -eq '-' }).Pos
           }
-        }
-        else {
-          $ThreatRowProp = @{
-            ThreatReal = $false
-            RowIndexUnderThreat = $Index
-            EmptyPos = ($RowObjects | Where-Object {$_.Content -eq '-' }).Pos
-          }         
+          $Result += (New-Object -TypeName psobject -Property $ThreatRow)
         }
       } #end if
-      New-Object -TypeName psobject -Property $ThreatRowProp
+      else {
+        [hashtable]$ThreatRow = @{
+          ThreatReal = $false
+          RowIndex = $Index
+          EmptyPos = ($RowObjects | Where-Object {$_.Content -eq '-' }).Pos
+        }         
+        $Result += (New-Object -TypeName psobject -Property $ThreatRow)
+      }
     } #end foreach
+    return $Result
   } #end Method
 } #END Class
 
+#Functions
+function Show-Board {
+  Param ([TTTBoardCell[]]$Board,[string]$Padding='  ')
+
+  Clear-Host
+  $EntryColors = @('White','White','White','White','White','White','White','White','White')
+  $ShowSqr = @(' ',' ',' ',' ',' ',' ',' ',' ',' ')
+  $GridColor = "Yellow"
+  $XColor = "Red"
+  $OColor = "white"
+  foreach ($Pos in (0..8)){
+    if ($Board[$Pos].Content -eq "X") { $EntryColors[$Pos] = $XColor}
+    if ($Board[$Pos].Content -eq "O") { $EntryColors[$Pos] = $OColor}
+    if ($Board[$Pos].Content -eq "-") { $EntryColors[$Pos] = "darkgray"}
+    if ($Board[$Pos].Content -match "[XO]") {$ShowSqr[$Pos] = $Board[$Pos].Content}
+    else {$ShowSqr[$Pos] = $Pos + 1}
+  }
+  Write-Host -ForegroundColor 'Yellow' "`n${Padding}Tic Tac Toe`n"
+  Write-Host -NoNewline "$Padding "
+  Write-Host -ForegroundColor $EntryColors[0] -NoNewline $ShowSqr[0]
+  Write-Host -ForegroundColor $GridColor -NoNewline " | "  
+  write-host -ForegroundColor $EntryColors[1] -NoNewline $ShowSqr[1]
+  write-host -ForegroundColor $GridColor -NoNewline " | "
+  Write-Host -ForegroundColor $EntryColors[2] $ShowSqr[2]
+  Write-Host -ForegroundColor $GridColor "${Padding}---+---+---"
+  Write-Host -NoNewline "$Padding "
+  Write-Host -ForegroundColor $EntryColors[3] -NoNewline $ShowSqr[3]
+  Write-Host -ForegroundColor $GridColor -NoNewline " | "  
+  write-host -ForegroundColor $EntryColors[4] -NoNewline $ShowSqr[4]
+  write-host -ForegroundColor $GridColor -NoNewline " | "
+  Write-Host -ForegroundColor $EntryColors[5] $ShowSqr[5]
+  Write-Host -ForegroundColor $GridColor "${Padding}---+---+---"
+  Write-Host -NoNewline "$Padding "
+  Write-Host -ForegroundColor $EntryColors[6] -NoNewline $ShowSqr[6]
+  Write-Host -ForegroundColor $GridColor -NoNewline " | "  
+  write-host -ForegroundColor $EntryColors[7] -NoNewline $ShowSqr[7]
+  write-host -ForegroundColor $GridColor -NoNewline " | "
+  Write-Host -ForegroundColor $EntryColors[8] $ShowSqr[8]
+  Write-Host 
+}
+
 #Main code
-
+Clear-Host
 [TTTBoardCell[]]$TTTBoard = foreach ($Pos in (0..8)) {[TTTBoardCell]::New($Pos)}
+Show-Board -Board $TTTBoard
+$Turn = 'X'
 do {
-  $BoardChoice = Read-Host 'Enter the choice'
-  $Pos = $BoardChoice -as [int]
-} until ($true)
-$TTTBoard[1].PlaceChoice('X')
-$TTTBoard[0].PlaceChoice('X')
-
-$TTTBoard | ft 
+  do {
+    $BadChoice = $false
+    $BoardChoice = Read-Host 'Enter the choice'
+    $ErrorActionPreference = 'stop'
+    try {[int]$ChoiceIndex = $BoardChoice - 1}
+    catch {$BadChoice = $true}
+    finally {$ErrorActionPreference = 'Continue'}
+  } until ($TTTBoard[$ChoiceIndex].PlaceChoice($Turn) -and $BadChoice -eq $false)
+  Show-Board -Board $TTTBoard
+  $Turn = @('X','O') | Where-Object {$_ -ne $Turn}
+} until ($TTTBoard.Content -notcontains '-')
