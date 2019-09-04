0a96a8bb30ba12301bf837d059db846c210c2a3b
Brent Denny <brent.denny@ddls.com.au>
2019-09-04 19:48:17 +1000

    new ttt problems with diag win

--- TTT/Play-TTTv2.ps1
+++ TTT/Play-TTTv2.ps1
@@ -2,9 +2,11 @@ Class TTTBoardCell {
   [int]$Pos
   [int]$Col
   [int]$Row
-  [Int]$Diag # Diag = 0 => 0-5-8 , Diag = 1 => 2,5,6
+  [Int[]]$Diag # Diag = 0 => 0-5-8 , Diag = 1 => 2,5,6 , Diag = 9 => not on a diagonal
   [string]$Content
-  [bool]$Threat
+  [bool]$ThreatO
+  [bool]$ThreatX
+  [bool]$Winner
 
   TTTBoardCell($Position) {
     $ColNum = $Position % 3
@@ -13,9 +15,13 @@ Class TTTBoardCell {
     $this.Col = $ColNum
     $this.Row = $RowNum
     $this.Content = '-'
-    if ($Position -in @(0,5,8) ) {$this.Diag = 0}
-    if ($Position -in @(2,5,6) ) {$this.Diag  = 1}
-    $this.Threat = $false
+    if ($Position -in @(0,8)) {$this.Diag = @(0)}
+    elseif ($Position -in @(2,6) ) {$this.Diag  = @(1)}
+    elseif ($Position -eq 4) {$this.Diag = @(0,1)}
+    else {$this.Diag = 9}
+    $this.ThreatO = $false
+    $this.ThreatX = $false
+    $this.Winner = $false
   }
    
   [bool]PlaceChoice([string]$Char) {
@@ -27,61 +33,61 @@ Class TTTBoardCell {
     else {return $false}
   }
   
-  [psobject[]]Threats([TTTBoardCell[]]$Board,[string]$TurnLetter) {
-    foreach ($Direction in @(0..7)) {
-      switch ($Direction) {
-        0 {$DirectionObjects = $Board | Where-Object {$_.Row -eq 0}}
-        1 {$DirectionObjects = $Board | Where-Object {$_.Row -eq 1}}
-        2 {$DirectionObjects = $Board | Where-Object {$_.Row -eq 2}}
-        3 {$DirectionObjects = $Board | Where-Object {$_.Col -eq 0}}
-        4 {$DirectionObjects = $Board | Where-Object {$_.Col -eq 1}}
-        5 {$DirectionObjects = $Board | Where-Object {$_.Col -eq 2}}
-        6 {$DirectionObjects = $Board | Where-Object {$_.Diag -eq 0}}
-        7 {$DirectionObjects = $Board | Where-Object {$_.Diag -eq 1}}
-      } #END Switch
-    } #END Foreach
-  } #END Threats Method
-  
-  
-
-    [psobject[]]$Result = @()
-    $OppositeLetter = @('X','O') | Where-Object {$_ -ne $TurnLetter}
-    $DirectionNames = @('Row','Col','Diag')
-    foreach ($Direction in (0..2)) {
-      if ($Direction -in (0..1)) {$IndexArray = @(0..2)}
-      else {$IndexArray = @(0..1)}
-      foreach ($Index in $IndexArray) {
-        $DirectionObjects = $Board | Where-Object {$_.Row -eq $Index}
-        $NumberOfBlanks = ($DirectionObjects | Group-Object | Where-Object {$_.Content -eq '-'}).count
-        $ThreatProperties = @{
-          ThreatReal = $false
-          RowIndex = $Index
-          EmptyPos = ($DirectionObjects | Where-Object {$_.Content -eq '-' }).Pos
-        } 
-        if ($NumberOfBlanks -eq 1) {
-          $NumberOfOpposite = ($DirectionObjects | Group-Object | Where-Object {$_.Content -eq $OppositeLetter[0]}).count
-          if ($NumberOfOpposite -eq 2) {
-            [hashtable]$ThreatProperties = @{
-              ThreatDirection = $DirectionNames[$Direction]
-              ThreatStatus = $true
-              ThreatIndex = $Index
-              EmptyPos = ($DirectionObjects | Where-Object {$_.Content -eq '-' }).Pos
-            }
-            $Result += (New-Object -TypeName psobject -Property $ThreatProperties)
-          }
-        } #end if
-        else {
-          [hashtable]$ThreatProperties = @{
-            ThreatReal = $false
-            RowIndex = $Index
-            EmptyPos = ($DirectionObjects | Where-Object {$_.Content -eq '-' }).Pos
-          }         
-          $Result += (New-Object -TypeName psobject -Property $ThreatProperties)
+  [void]UpdateStatus([TTTBoardCell[]]$Board,[string]$TurnLetter) {
+    $OpponentLettter = (@('X','O') | Where-Object {$_ -ne $TurnLetter})[0]
+    $RowCells  = $Board | Where-Object {$_.Row -eq $this.Row}
+    $CountXinRow = ($RowCells | Where-Object {$_.Content -eq 'X'}).Count
+    $CountOinRow = ($RowCells | Where-Object {$_.Content -eq 'O'}).Count
+    $ColCells  = $Board | Where-Object {$_.Col -eq $this.Col}
+    $CountXinCol = ($ColCells | Where-Object {$_.Content -eq 'X'}).Count
+    $CountOinCol = ($ColCells | Where-Object {$_.Content -eq 'O'}).Count
+    $DiagCells0 = $Board | Where-Object {$_.Diag -contains 0}
+    $CountXinDiag0 = ($DiagCells0 | Where-Object {$_.Content -eq 'X'}).Count
+    $CountOinDiag0 = ($DiagCells0 | Where-Object {$_.Content -eq 'O'}).Count
+    $DiagCells1 = $Board | Where-Object {$_.Diag -contains 1}
+    $CountXinDiag1 = ($DiagCells1 | Where-Object {$_.Content -eq 'X'}).Count
+    $CountOinDiag1 = ($DiagCells1 | Where-Object {$_.Content -eq 'O'}).Count
+    if ($this.Content -eq '-'){
+      if ($CountXinRow -eq 2 ){$this.ThreatO = $true}
+      if ($CountOinRow -eq 2 ){$this.ThreatX = $true}
+      if ($CountXinCol -eq 2 ){$this.ThreatO = $true}
+      if ($CountOinCol -eq 2 ){$this.ThreatX = $true}
+      if (($ColCells.Content | Where-Object {$_.Content -eq $OpponentLettter}).count -eq 2){
+        if ($TurnLetter -eq 'X')     {$this.ThreatX = $true}
+        elseif ($TurnLetter -eq 'O') {$this.ThreatO = $true}
+      }      
+      if ($this.Diag -contains 0) {
+        if ($CountXinDiag0 -eq 2 ){$this.ThreatO = $true}
+        if ($CountOinDiag0 -eq 2 ){$this.ThreatX = $true}
+        if (($DiagCells0.Content | Where-Object {$_.Content -eq $OpponentLettter}).count -eq 2){
+          if ($TurnLetter -eq 'X')     {$this.ThreatX = $true}
+          elseif ($TurnLetter -eq 'O') {$this.ThreatO = $true}
         }
-      } #end foreach
-    } #End foreach
-    return $Result
-  } #end Method
+      }
+      if ($this.Diag -contains 1) {
+        if ($CountXinDiag1 -eq 2 ){$this.ThreatO = $true}
+        if ($CountOinDiag1 -eq 2 ){$this.ThreatX = $true}
+        if (($DiagCells1.Content | Where-Object {$_.Content -eq $OpponentLettter}).count -eq 2){
+          if ($TurnLetter -eq 'X')     {$this.ThreatX = $true}
+          elseif ($TurnLetter -eq 'O') {$this.ThreatO = $true}
+        }
+      }
+    }
+    else {
+      $this.ThreatX = $false
+      $this.ThreatO = $false
+      #Check for winner
+      if ($CountXinRow -eq 3) {$this.Winner = $true}
+      if ($CountOinRow -eq 3) {$this.Winner = $true} 
+      if ($CountXinCol -eq 3) {$this.Winner = $true}
+      if ($CountOinCol -eq 3) {$this.Winner = $true}
+      if ($CountXinDiag0 -eq 3) {$this.Winner = $true}
+      if ($CountOinDiag0 -eq 3) {$this.Winner = $true}
+      if ($CountXinDiag1 -eq 3) {$this.Winner = $true}
+      if ($CountOinDiag1 -eq 3) {$this.Winner = $true} 
+      
+    }
+  } #END DetectThreat Method
 } #END Class
 
 #Functions
@@ -133,12 +139,14 @@ $Turn = 'X'
 do {
   do {
     $BadChoice = $false
-    $BoardChoice = Read-Host 'Enter the choice'
+    $BoardChoice = Read-Host "$Turn's turn - Enter the choice"
     $ErrorActionPreference = 'stop'
     try {[int]$ChoiceIndex = $BoardChoice - 1}
     catch {$BadChoice = $true}
     finally {$ErrorActionPreference = 'Continue'}
   } until ($TTTBoard[$ChoiceIndex].PlaceChoice($Turn) -and $BadChoice -eq $false)
   Show-Board -Board $TTTBoard
+  $TTTBoard | ForEach-Object {$_.UpdateStatus($TTTBoard,$Turn)}
+  $TTTBoard | ft
   $Turn = @('X','O') | Where-Object {$_ -ne $Turn}
-} until ($TTTBoard.Content -notcontains '-')
+} until ($TTTBoard.Content -notcontains '-' -or $TTTBoard.Winner -contains $true)
