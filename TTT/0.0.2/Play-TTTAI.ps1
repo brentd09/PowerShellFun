058445323cc44f583d3127c735ab7776f26d8b51
Brent Denny <brent.denny@ddls.com.au>
2020-01-13 23:54:05 +1000

    setting up classes for ttt

--- TTT/0.0.2/Play-TTTAI.ps1
+++ TTT/0.0.2/Play-TTTAI.ps1
@@ -2,6 +2,16 @@
 
 # Class definitions
 
+Class ThreatList {
+  [int[]]$XThreats
+  [int[]]$OThreats
+
+  ThreatList () {
+    $this.XThreats = @()
+    $this.OThreats = @()
+  }
+}
+
 Class TTTBoardCell {
   [int]$Position
   [string]$Value 
@@ -25,11 +35,49 @@ Class TTTBoardCell {
 
 class TTTBoard {
   [TTTBoardCell[]]$Cells
-  [string]$Winner 
 
   TTTBoard () {
     $this.Cells = foreach ($Pos in (0..8)) { [TTTBoardCell]::New($Pos)}
-    $this.Winner = 'N'
+  }
+
+  [string]TestWin () {
+    [System.Collections.ArrayList]$WinningLines = @(
+      @(0,1,2),@(3,4,5),@(6,7,8),
+      @(0,3,6),@(1,4,7),@(2,5,8),
+      @(0,4,8),@(2,4,6)
+    )
+    [string]$Win = 'N'
+    foreach ($Line in $WinningLines) {
+      if ($this.Cells[$Line[0]].Value -eq $this.Cells[$Line[1]].Value -and $this.Cells[$Line[0]].Value -eq $this.Cells[$Line[2]].Value) {
+        $Win = $this.Cells[$Line[0]].Value
+        break
+      }
+    }
+    return $Win
+  }
+
+  [ThreatList]TestThreat () {
+    [System.Collections.ArrayList]$WinningLines = @(
+      @(0,1,2),@(3,4,5),@(6,7,8),
+      @(0,3,6),@(1,4,7),@(2,5,8),
+      @(0,4,8),@(2,4,6)
+    )
+    $Threats = [ThreatList]::new()
+    foreach ($Line in $WinningLines) {
+      [string[]]$XsInLine = ($this.Cells[$Line].Value) -eq 'X'
+      [string[]]$OsInLine = ($this.Cells[$Line].Value) -eq 'O'
+      if ($XsInLine.count -eq 2 -and $OsInLine.Count -eq 0) {
+        $ThreatPosString = ($this.Cells[$Line].Value) -ne 'X'
+        $ThreatPos = ($ThreatPosString[0] -as [int]) -1
+        $Threats.XThreats += $ThreatPos
+      }
+      if ($OsInLine.Count -eq 2 -and $XsInLine.Count -eq 0) {
+        $ThreatPosString = ($this.Cells[$Line].Value) -ne 'O'
+        $ThreatPos = ($ThreatPosString[0] -as [int]) -1
+        $Threats.OThreats += $ThreatPos
+      }
+    }
+    return $Threats
   }
 }
 
@@ -44,7 +92,7 @@ Function Test-TerminalState {
   )
   foreach ($Line in $WinningLines) {
     # Check to see if all three places have the same Value (X or O)
-    if ($GameBoard.Cells[$Line[0]])
+  
   }
 }
 
