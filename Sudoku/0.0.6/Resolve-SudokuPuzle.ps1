b245b77e76a3660e1f68208695cdde21e48ce697
Brent Denny <brent.denny@ddls.com.au>
2020-03-04 16:55:36 +1000

    sudoku solves up to difficult now

--- Sudoku/0.0.6/Resolve-SudokuPuzle.ps1
+++ Sudoku/0.0.6/Resolve-SudokuPuzle.ps1
@@ -1,4 +1,37 @@
-
+<#
+.SYNOPSIS
+  Solve Sudoku puzzles
+.DESCRIPTION
+  This will attempt to solve sudoku puzzles by the offical methods: sole candidate, hidden candidate, etc.
+  Here are some puzzle strings to try:
+  '7-542--6-68-1--24--4-76--18-91--2-7482--576--3---1482-158--6--9--25-91-6--684-7-2' - EASY
+  '-9--2-5----4--5-1--6-----93--18---6----9----2-8--72---5----1-7----3--9-1-13------' - Medium
+  '-9--2-5----4--5-1--6-----93--18---6----9----2-8--72---5----1-7----3--9-1--3------' - Difficult
+  '--97486--7---------2-1-987---7---24--64-1-59--98---3-----8-3-2---------6---2759--' - Difficult
+  '-714---------17--59------4-5-8-6341--3--------9-----28-----4-6--6--89--1----3--5-' - Difficult
+  '-2-6------562-----1------28----2-4-9-914-873-2-8-9----71------3-----217------5-6-' - Difficult
+  '89-2-3-------------3658---41-8-3--6-----------2--7-3-57---9412-------------8-2-59' - Extreme
+  '--9748---7---------2-1-9-----7---24--64-1-59--98---3-----8-3-2---------6---2759--' - Impossible
+  '-714---------17--59------4-5-8-634---3--------9-----28-----4-6--6--89--1----3--5-' - Impossible
+  '----15-74----3-8---87---5-1-23--4----1--7--2----2--79-8-6---24---1-2----23-64----' - impossible
+  '--7---28---4-25---28---46---9---6---3-------2---1---9---62---75---57-4---78---3--' - impossible 
+  '1-----569492-561-8-561-924---964-8-1-64-1----218-356-4-4-5---1-9-5-614-2621-----5' - impossible (xwing)
+.EXAMPLE
+  PS C:\> <example usage>
+  Explanation of what the example does
+.INPUTS
+  Inputs (if any)
+.OUTPUTS
+  Output (if any)
+.NOTES
+  General notes
+#>
+[CmdletBinding()]
+Param (
+  [string]$PuzzleString = '89-2-3-------------3658---41-8-3--6-----------2--7-3-57---9412-------------8-2-59',
+  [switch]$ShowRawData
+)
+# Class Definitions
 class SudokuElement {
   [int]$Position 
   [string]$Value
@@ -35,7 +68,124 @@ class SudokuBoard {
     $this.Board = $Elements
     $this.UnsolvedElements = ($Elements | Where-Object {$_.Value -match '\D'}).count
   }
+  
+  [void]ResolveUnsolved () {
+    $this.UnsolvedElements = ($this.Board | Where-Object {$_.Value -match '\D'}).count
+  }
 }
 
-$test = [SudokuBoard]::New('7-542--6-68-1--24--4-76--18-91--2-7482--576--3---1482-158--6--9--25-91-6--684-7-2')
-$test
+# Function Definitions
+function Compare-Arrays {  
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string[]]$Array1,
+    [Parameter(Mandatory=$true)]
+    [string[]]$Array2
+  )
+  $A1inA2    = @()
+  $A1notinA2 = @()
+  $A2inA1    = @()
+  $A2notinA1 = @()
+  
+  foreach ($Array1Element in $Array1) {
+    if ($Array1Element -in $Array2) {$A1inA2 += $Array1Element}
+    else {$A1notinA2 += $Array1Element}
+  }
+  foreach ($Array2Element in $Array2) {
+    if ($Array2Element -in $Array1) {$A2inA1 += $Array2Element}
+    else {$A2notinA1 += $Array2Element}
+  }
+  $ObjHash = [ordered]@{
+    Array1 = $Array1
+    Array2 = $Array2
+    ElementsCommon = $A1inA2
+    UniqueToArray1 = $A1notinA2
+    UniqueToArray2 = $A2notinA1
+  }
+  New-Object -TypeName psobject -Property $ObjHash
+}
+function Resolve-NumbersMissing {
+  Param (
+    [SudokuBoard]$FnSudoku
+  )
+  foreach ($Index in (0..80)) {
+    if ($FnSudoku.Board[$index].Value -match '\D') {
+      $RelatedCol = $FnSudoku.Board[$index].Col
+      $RelatedRow = $FnSudoku.Board[$index].Row
+      $RelatedSqr = $FnSudoku.Board[$index].Sqr
+      $AllRelatedValues = $FnSudoku.board | 
+       Where-Object {$_.Value -match '\d' -and ($_.Row -eq $RelatedRow -or $_.Col -eq $RelatedCol -or $_.Sqr -eq $RelatedSqr)}
+      $UniqueRelatedValues = $AllRelatedValues.Value | Select-Object -Unique | Sort-Object 
+      $NumbersMissing = (Compare-Arrays -Array1 (1..9) -Array2 $UniqueRelatedValues).UniqueToArray1
+      if ($NumbersMissing.count -eq 1) {$FnSudoku.Board[$Index].Value = $NumbersMissing[0]} # Sole Candidate
+      $FnSudoku.Board[$index].PossibleValues = $NumbersMissing
+    }
+    $FnSudoku.ResolveUnsolved()
+  }
+}
+
+function Show-Sudoku {
+  Param (
+    [SudokuBoard]$fnSudoku,
+    [Switch]$RawData
+  )
+  if ($RawData -eq $true) {$fnSudoku.Board | format-table}
+  else {
+    $Coords = New-Object -TypeName System.Management.Automation.Host.Coordinates
+    $host.UI.RawUI.CursorPosition = $Coords
+    $FGColor = 'Yellow'
+    Write-Host -ForegroundColor Green "     Solve Sudoku`n"
+    foreach ($PosCol in (0..8)) {
+      if ($PosCol -eq 2 -or $PosCol -eq 5) {$HBdr = "`n------+-------+------"}
+      else {$HBdr = ''}
+      foreach ($PosRow in (0..8)) {
+        if ($PosRow -eq 2 -or $PosRow -eq 5) {$Bdr = ' | '}
+        else {$Bdr = ' '}
+        Write-Host -NoNewline $fnSudoku.Board[($PosRow+($PosCol*9))].Value
+        Write-Host -NoNewline -ForegroundColor $FGColor "$Bdr"
+      }
+      Write-Host -ForegroundColor $FGColor $HBdr
+    }
+    Write-Host
+    #Start-Sleep -Seconds 1
+  }
+}
+
+function Resolve-Unique {
+  param (
+    [SudokuBoard]$fnSudoku
+  )
+  foreach ($Index in (0..8)) {
+    $SqrCells = $fnSudoku.Board | Where-Object {$_.Sqr -eq $Index -and $_.Value -eq '-'}
+    $SqrCellsValues =  $SqrCells.PossibleValues 
+    $UniqueValues = ($SqrCellsValues | Sort-Object | Group-Object | Where-Object {$_.count -eq 1}).Name
+    foreach ($UniqueValue in $UniqueValues) {
+      $WhichCell = $SqrCells | Where-Object {$_.PossibleValues -contains $UniqueValue}
+      $fnSudoku.Board[$WhichCell.Position].Value = $UniqueValue
+    } # Unique Candidate
+  }
+}
+
+function Find-Impossible {
+  Param (
+    [SudokuBoard]$fnSudoku
+  )
+  
+}
+# Main Code
+Clear-Host
+$Puzzle = [SudokuBoard]::New($PuzzleString)
+if ($ShowRawData) {Show-Sudoku -fnSudoku $Puzzle -RawData}
+else {Show-Sudoku -fnSudoku $Puzzle}
+do { 
+  $UnsolvedBefore = $Puzzle.UnsolvedElements
+  Resolve-NumbersMissing -fnSudoku $Puzzle
+  $UnsolvedAfter = $Puzzle.UnsolvedElements
+  if ($ShowRawData) {Show-Sudoku -fnSudoku $Puzzle -RawData}
+  else {Show-Sudoku -fnSudoku $Puzzle}
+  if ($UnsolvedBefore -eq $UnsolvedAfter) {
+    Resolve-Unique -fnSudoku $Puzzle
+  } 
+} until ($Puzzle.UnsolvedElements -eq 0 )  
+
