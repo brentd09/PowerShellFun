31f1ee0ac028fba00bbc3c598eaa08ace1326d2b
Brent Denny <brent.denny@ddls.com.au>
2022-01-18 00:43:04 +1000

    fixed the code for rummy

--- Cards/Play-Rummy.ps1
+++ Cards/Play-Rummy.ps1
@@ -2,13 +2,15 @@ Class PlayingCard {
   [int]$RankValue
   [string]$Rank
   [string]$Suit
+  [string]$SuitName
   [string]$Color
   [int]$ScoreValue
 
-  PlayingCard ([string]$FaceRank, [int]$FaceValue, [string]$FaceSuit, [string]$FaceColor, [int]$ScoreVal) {
+  PlayingCard ([string]$FaceRank, [int]$FaceValue, [string]$FaceSuit, [string]$SuitName, [string]$FaceColor, [int]$ScoreVal) {
     $this.Rank       = $FaceRank
     $this.RankValue  = $FaceValue
     $this.Suit       = $FaceSuit
+    $this.SuitName   = $SuitName
     $this.Color      = $FaceColor
     $this.ScoreValue = $ScoreVal
   }
@@ -19,27 +21,6 @@ Class PlayingCard {
 
 }
 
-function New-CardPack {
-  [int[]]$SuitASCIINumbers = @(9824,9827,9829,9830)
-  $RankNumbers = 1..13
-  foreach ($SuitASCIINumber in $SuitASCIINumbers) {
-    if ($SuitASCIINumber -in  @(9824,9827) ) {$Color = 'Black'}
-    else {$Color = 'Red'}
-    $Suit = [char]$SuitASCIINumber
-    foreach ($Rank in $RankNumbers) {
-      switch ($Rank) {
-         1 { $RankString = 'A'; $ScoreVal = 15 }
-        11 { $RankString = 'J'; $ScoreVal = 10 }
-        12 { $RankString = 'Q'; $ScoreVal = 10 }
-        13 { $RankString = 'K'; $ScoreVal = 10 }
-        Default {$RankString = $Rank -as [string]; $ScoreVal = 5}
-      }
-      $Card = [PlayingCard]::New($RankString,$Rank,$Suit,$Color,$ScoreVal)
-      $Card
-    }
-  }
-}
-
 Class PlayingDeck {
   [System.Collections.ArrayList]$Cards
 
@@ -48,8 +29,10 @@ Class PlayingDeck {
       [int[]]$SuitASCIINumbers = @(9824,9827,9829,9830)
       $RankNumbers = 1..13
       foreach ($SuitASCIINumber in $SuitASCIINumbers) {
-        if ($SuitASCIINumber -in  @(9824,9827) ) {$Color = 'Black'}
-        else {$Color = 'Red'}
+        if ($SuitASCIINumber -eq  9824) {$Color = 'Black';$SuitName = 'Spades'}
+        elseif ($SuitASCIINumber -eq  9827) {$Color = 'Black';$SuitName = 'Clubs'}
+        elseif ($SuitASCIINumber -eq  9829) {$Color = 'Red';$SuitName = 'Hearts'}
+        elseif ($SuitASCIINumber -eq  9830) {$Color = 'Red';$SuitName='Diamonds'}
         $Suit = [char]$SuitASCIINumber
         foreach ($Rank in $RankNumbers) {
           switch ($Rank) {
@@ -59,8 +42,7 @@ Class PlayingDeck {
             13 { $RankString = 'K'; $ScoreVal = 10 }
             Default {$RankString = $Rank -as [string]; $ScoreVal = 5}
           }
-          $Card = [PlayingCard]::New($RankString,$Rank,$Suit,$Color)
-          $Card.SetScoreVal($ScoreVal)
+          $Card = [PlayingCard]::New($RankString,$Rank,$Suit,$SuitName,$Color,$ScoreVal)
           $Card
         }
       }
@@ -81,8 +63,17 @@ Class PlayingDeck {
 
 
 ### Main Code
-
+[System.Collections.ArrayList]$Player1 = @()
+[System.Collections.ArrayList]$Player2 = @()
 $Deck = [PlayingDeck]::New()
 $Deck.ShuffleDeck()
-$Deck.DealFromDeck()
+
+#Initial Deal
+0..6 | ForEach-Object {
+  $Player1 += $Deck.DealFromDeck()
+  $Player2 += $Deck.DealFromDeck()
+}
+$Player1 | ft
+Write-Output '---------------------'
+$Player2 | ft
 $Deck.Cards
