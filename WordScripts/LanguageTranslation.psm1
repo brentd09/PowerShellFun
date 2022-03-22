c0ff3bdac8825aef253659cfc64d11eef7b9e6b7
Brent Denny <brent.denny@ddls.com.au>
2022-03-22 17:50:58 +1000

    piglatin

---
+++ WordScripts/LanguageTranslation.psm1
@@ -0,0 +1,25 @@
+﻿function ConvertTo-PigLatin {
+  [CmdletBinding()]
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string]$Sentence
+  )
+  $Words = $Sentence -split '\s+'
+  Write-Verbose ($Words -join " ")
+  [string[]]$NewWords = @()
+  foreach ($Word in $Words) {
+    $Word = $Word.tolower()
+    Write-Verbose $Word
+    switch -regex ($Word) {
+        '^[aeiou]' {
+          $NewWords += $Word+'yay'
+        }
+        '^[bcdfghjklmnpqrstuvwxyz]' {
+          $NewWords += $Word -replace '^([bcdfghjklmnpqrstuvwxyz]+)([aeiou]\w*)$','$2$1ay'
+        }
+        Default {}
+    }
+  }    
+  return $NewWords -join ' '
+}
+
