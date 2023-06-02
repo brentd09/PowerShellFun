6c7b416d6740d44b89a912d6428d1cbb9a07f50d
Brent Denny <brent.denny@lumifygroup.com>
2023-06-02 16:44:32 +1000

    added last changes to GOL

--- GameOfLife/1.0.0/Play-GameOfLife.ps1
+++ GameOfLife/1.0.0/Play-GameOfLife.ps1
@@ -1,4 +1,36 @@
+<#
+.SYNOPSIS
+  Game of Life
+.DESCRIPTION
+  This simulates the game of life: Conway's GOL
+  There are three rules:
+  - A live position will remain living if it has two or three neighbours
+  - An empty position will come to life if it has three neighbours
+  - All other positions wil either die or remain empty if none of the previous rules apply 
+.NOTES
+  Created By:   Brent Denny
+  Created On:   01-Jun-2023
+  Last Edited:  02-Jun-2023
+  
+  Change History:
+  Date         Details
+  01-Jun-2023  Created the basics regarding the multi-dimentional array
+  02-Jun-2023  Added the logic to test for neighbours and check next cycle states
+               Added the method to change values based on NextState directives
 
+.EXAMPLE
+  Play-GameOfLife -MaxSize 15
+  This will play out a game of life on a 15 x 15 size grid the game will continue 
+  for six times the grid size or if it reaches a stable state where nothing changes
+  
+#>
+[CmdletBinding()]
+Param (
+  [int]$MaxSize = 20
+)
+
+
+#Classes
 class Element {
   [string]$Value
   [string]$NextState  
@@ -14,6 +46,7 @@ class Element {
   }
 }
 
+# Functions
 function Show-Grid {
   Param (
     [Element[,]]$FGrid
@@ -21,7 +54,7 @@ function Show-Grid {
   
   foreach ($Row in @(0..$MaxIndex)){
     foreach ($Col in @(0..$MaxIndex)) {
-      if ($FGrid[$Row,$Col].Value -eq '#') {$Color = 'Yellow'} else {$Color = 'White'}
+      if ($FGrid[$Row,$Col].Value -eq '#') {$Color = 'Cyan'} else {$Color = 'White'}
       Write-Host -ForegroundColor $Color -NoNewline $FGrid[$Row,$Col].Value
       Write-Host -NoNewline '  '
     }
@@ -47,8 +80,11 @@ function Find-Neighbour {
   return $RowColNeighbours
 }
 
-$MaxSize  = 20
+
+## Main Code
 $MaxIndex = $MaxSize - 1 
+$TurnCount = 0
+$MaxTurns = 6 * $MaxSize
 
 
 $Grid = [Element[,]]::New($MaxSize,$MaxSize)
@@ -58,11 +94,14 @@ foreach ($Row in @(0..$MaxIndex)){
     $Grid[$Row,$Col] = [element]::New($CellValue,'*')
   }
 }
-
+$TopOfScreen = [System.Management.Automation.Host.Coordinates]::New(0,0)
+Clear-Host
 do {
-  Clear-Host
+  $Host.UI.RawUI.CursorPosition = $TopOfScreen
   [string]$GridPattern = $Grid.Value
   Show-Grid -FGrid $Grid
+  Write-Host 
+  Write-Host "Count: $TurnCount"
   foreach ($Row in @(0..$MaxIndex)){
     foreach ($Col in @(0..$MaxIndex)) {
       $Nbours = Find-Neighbour -FGrid $Grid -Row $Row -Col $Col
@@ -78,6 +117,7 @@ do {
       $Grid[$Row,$Col].CalcNextCycle()
     }
   }
-  Start-Sleep -Seconds 1
+  #Start-Sleep -Milliseconds 100
   [string]$PostGridPattern = $Grid.Value
-} until ($GridPattern -eq $PostGridPattern)
+  $TurnCount++
+} until ($GridPattern -eq $PostGridPattern -or $TurnCount -gt $MaxTurns)
