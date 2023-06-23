7d673f8e24bec5ae16eeae5157f60e30daa24862
Brent Denny <brent.denny@lumifygroup.com>
2023-06-23 22:39:01 +1000

    Othello taking shape

--- Reverse/1.0.0/Play-Othello.ps1
+++ Reverse/1.0.0/Play-Othello.ps1
@@ -1,90 +1,63 @@
-$Grid = @(
-  @(' ',' ',' ',' ',' ',' ',' ',' '),
-  @(' ',' ',' ',' ',' ',' ',' ',' '),
-  @(' ',' ',' ',' ',' ',' ',' ',' '),
-  @(' ',' ',' ','X','O',' ',' ',' '),
-  @(' ',' ',' ','O','X',' ',' ',' '),
-  @(' ',' ',' ',' ',' ',' ',' ',' '),
-  @(' ',' ',' ',' ',' ',' ',' ',' '),
-  @(' ',' ',' ',' ',' ',' ',' ',' ')
-)
+Class OthelloPosition {
+  [string]$Value
+  [int]$Index
+  [int]$Row
+  [int]$Col
+  [int[]]$Neighbours
 
-# Functions
-function Show-Grid {
-  Param ($Grid)
+  OthelloPosition ($Index,$Val) {
+    [int[]]$RelativeNeighbours = @()
+    $this.Value = $Val
+    $this.Row = [math]::Truncate($Index / 8)
+    $this.Col = $Index % 8
+    foreach ($pos in (0..7)) {
+      $PossibleNeighbours = @(-9,-8,-7,-1,+1,+7,+8,+9)
+      if ($Index -eq 0) {$RelativeNeighbours = $PossibleNeighbours[4,6,7]}
+      elseif ($Index -eq 7) {$RelativeNeighbours = $PossibleNeighbours[3,5,6]}
+      elseif ($Index -eq 56) {$RelativeNeighbours = $PossibleNeighbours[1,2,4]}
+      elseif ($Index -eq 63) {$RelativeNeighbours = $PossibleNeighbours[0,1,3]}
+      elseif ($Index -in @(8,16,24,32,40,48)) {$RelativeNeighbours = $PossibleNeighbours[1,2,4,6,7]}
+      elseif ($Index -in @(15,23,31,39,47,55)) {$RelativeNeighbours = $PossibleNeighbours[0,1,3,5,6]}
+      elseif ($Index -in @(1,2,3,4,5,6)) {$RelativeNeighbours = $PossibleNeighbours[3..7]}
+      elseif ($Index -in @(57,58,59,60,61,62)) {$RelativeNeighbours = $PossibleNeighbours[0..4]}
+      else {$RelativeNeighbours = $PossibleNeighbours} 
+    }
+    $this.Neighbours = foreach ($RelativeNeighbour in $RelativeNeighbours) {$Index + $RelativeNeighbour}
+  }
+}
 
-  Write-Host "   A B C D E F G H"
-  foreach ($Row in @(0..7)) {
-    Write-Host "$($Row+1)  " -NoNewline
-    foreach ($Col in @(0..7)) {
-      if ($Grid[$Row][$Col] -in @('X','O')) {Write-Host "$($Grid[$Row][$Col]) " -NoNewline}
-      else {Write-Host ". " -NoNewline}
+Class OthelloBoard {
+  [OthelloPosition[]]$Positions 
+
+  OthelloBoard () {
+    $this.Positions = foreach ($Indx in 0..63) {
+      if ($Indx -in @(27,36)) {[OthelloPosition]::New($Indx,'X')}
+      elseif ($Indx -in @(28,35)) {[OthelloPosition]::New($Indx,'O')}
+      else  {[OthelloPosition]::New($Indx,'.')}
+    }
+  }
+
+  ShowBoard () {
+    $RowLabels = @('A','B','C','D','E','F','G','H')
+    $RowNum = 0
+    Write-Host -NoNewline -ForegroundColor Cyan "  1  2  3  4  5  6  7  8"
+    foreach ($Pos in @(0..63)) {
+      if (($Pos % 8) -eq 0) {
+        Write-Host
+        Write-Host -NoNewline -ForegroundColor Cyan "$($RowLabels[$RowNum])"
+        $RowNum++
+      }
+      Write-Host -NoNewline " $($this.Positions[$Pos].Value) "
     }
     Write-Host
-  }
-  Write-Host
-}
-
-function Find-Neighbours {
-  Param (
-    $Grid,
-    [int]$Row,
-    [int]$Col
-  )
-  [System.Collections.ArrayList]$Neighbours = @()
-  if (($Row - 1) -ge 0) { 
-    foreach ($c in @(-1,0,1)){
-      if (($Col + $c) -ge 0 -and ($Col + $c) -le 7) {
-        $Neighbours.add(@(($Row - 1), ($Col + $c))) | Out-Null
-      }
-    }
-  }
-  foreach ($c in @(-1,1)){
-    if (($Col + $c) -ge 0 -and ($Col + $c) -le 7) {
-      $Neighbours.add(@(($Row), ($Col + $c))) | Out-Null
-    }
-  }
-  if (($Row + 1) -ge 0) { 
-    foreach ($c in @(-1,0,1)){
-      if (($Col + $c) -ge 0 -and ($Col + $c) -le 7) {
-        $Neighbours.add(@(($Row + 1), ($Col + $c))) | Out-Null
-      }
-    }
-  }  
-  return $Neighbours
-}
-
-function Find-LegalMoves {
-  Param (
-    $Grid,
-    $Player
-  )
-  foreach ($Row in @(0..7)) {
-    foreach ($Col in @(0..7)) {
-      if ($Grid[$Row][$Col] -in @('X','O')) {continue}
-      $LegalNeighbours = Find-Neighbours -Grid $Grid -Row $Row -Col $Col
-      foreach ($Neighbour in $LegalNeighbours) {
-        if ($Grid[$Neighbour])
-      }
-    }
+    Write-Host
   }
 }
 
-# Main code
-Clear-Host
-$Player = 'X'
+# Main Code
+
+$Board = [OthelloBoard]::New()
 do {
-  Show-Grid -Grid $Grid
-  do {
-    $Choice = Read-Host -Prompt "Player $Player, enter a coordinate to move"
-    $ChoiceNumber = $Choice -replace '[^1-8]',''
-    $ChoiceLetter = ($Choice -replace '[^a-z]','').ToUpper()
-
-  } until ($ChoiceNumber -match '\d' -and $ChoiceLetter -match '[a-h]')
-  [int]$RowIndex = $ChoiceNumber - 1
-  [string]$ColIndex = ([int][char]$ChoiceLetter) - 65 
-  Find-Neighbours -Grid $Grid -Row $RowIndex -Col $ColIndex
-  
-
-  
-} until ($false)
+  $Board.ShowBoard()
+  break
+} Until ($true) #game over
